e3a5e7363986744d7e276981571a7d6e35b1dcb7
queerviolet <Ashi Krishnan me@ashi.works>
2017-03-21 10:50:20 -0400

    wip

--- hat/index.js
+++ hat/index.js
@@ -9,27 +9,35 @@ const user = Rx.Observable.create(obs =>
     : auth.signInAnonymously()))
 
 const home = user.map(user => db.ref('users').child(user.uid))
-const profile = home.map(user => Profile(user))
-
-const Profile = form({
+const Profile = model({
   name: 'Your name',
   familiarName: "Your familiar's name (if any)",
   dateOfBirth: 'Your birthday (if any)',
   dateOfFirstSpell: 'The date you cast your first spell, incantation, or enchantment',
 })
+const profile = Profile(home)
 
-// const Reference = Object.getPrototypeOf(db.ref())
-
-// Object.defineProperties(Reference, {
-//   values: {
-//     get() { return values(this) }
-//   },
-// })
-
-const values = ref => Rx.Observable.create(obs => {
-  const listener = ref.on('value', snap => obs.onNext(snap.val()))
-  return () => ref.off('value', listener)
-})
+function model(fields) {
+  const props = Object.keys(fields)
+    .reduce(
+      (props, field) => Object.assign({}, props, {
+        [field]: {
+          get() {
+            let _ref = null
+            const refRx = this.refRx.flatMapLatest(ref => {
+              _ref = ref
+              return values(ref.child(field))
+            })
+            return Object.defineProperties(Object.create(refRx), {
+              question: { get() { return fields[field] } },
+              ref: { get() { return _ref } }
+            })
+          },
+          set(val) { this.refRx.first().subscribe(ref => ref.child(field).set(val)) },
+        }
+      }), {})
+  return refRx => Object.defineProperties({refRx}, props)
+}
 
 function form(questions) {
   const props = Object.keys(questions)
@@ -48,12 +56,28 @@ function form(questions) {
   return ref => Object.defineProperties({ref}, props)
 }
 
-module.exports = {profile}
+function reset() {
+  auth.signOut()
+}
 
-home.flatMapLatest(home => values(home))
-  .subscribe(state => console.log('State:', state))
-
-profile.subscribe(hat => {
-  window.hat = hat
-  console.log('updated hat')
+const values = ref => Rx.Observable.create(obs => {
+  const listener = ref.on('value', snap => obs.onNext(snap.val()))
+  return () => ref.off('value', listener)
 })
+
+let _state = null
+home.flatMapLatest(home => values(home))
+  .subscribe(state => {
+    console.log('old state:', _state)
+    _state = state    
+    console.log('new state:', _state)
+  })
+
+module.exports = {
+  profile,
+  user,
+  reset,
+  get state() { return _state },
+}
+
+window.Hat = module.exports
