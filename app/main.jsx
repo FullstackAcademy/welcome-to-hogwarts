8bcefdd0c4adb297abe9bc886159c2c984b01a57
queerviolet <Ashi Krishnan me@ashi.works>
2017-03-21 11:29:24 -0400

    New welcome layout; uses the hat.

--- app/main.jsx
+++ app/main.jsx
@@ -4,24 +4,37 @@ import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
 import {render} from 'react-dom'
 
 import Hat from '../hat'
+
 import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 
-const ExampleApp =
-  ({ user, children }) =>
-    <div>
-      <nav>
-        {user ? <WhoAmI/> : <Login/>}
-      </nav> 
-      {children}
-    </div>
+class Layout extends React.Component {
+  componentDidMount() {
+    this.unsubscribe = Hat.profile.subscribe(user => this.setState({user}))
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
+
+  render() {
+    if (!this.state) return null
+    const {user} = this.state
+        , {children} = this.props
+    console.log(user)
+    const jsx =
+      <div>
+        <h1>Welcome, {user.name}</h1>
+        {children}
+      </div>
+    return jsx
+  }
+}
 
 render (
   <Router history={browserHistory}>
-    <Route path="/" component={ExampleApp}>
-      <IndexRedirect to="/jokes" />
-      <Route path="/jokes" component={Jokes} />
+    <Route path="/" component={Layout}>
     </Route>
   </Router>,
   document.getElementById('main')
